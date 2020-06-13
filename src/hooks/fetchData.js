7932f7731c18ac48ea7aa1ec08babcb244c859f7
Sérgio Costa <kostyny@protonmail.com>
2020-06-13 22:07:44 +0100

    feat(hook): Create service for fetch data

--- src/hooks/fetchData.js
+++ src/hooks/fetchData.js
@@ -1,29 +1,19 @@
 import { useEffect, useState } from 'react';
 import constants from '../constants/constants';
+import FetchEpisodes from '../services/fetchEpisodes';
 
 function useAsyncList(dispatch = false) {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
 
     useEffect(() => {
-        async function fetchEpisodes() {
-            try {
-                const data = await fetch(constants.URL_RICK_MORTY_EPISODES);
-                const dataJSON = await data.json();
-                setLoading(true);
-                return dispatch({
-                    type: 'FETCH_DATA',
-                    // eslint-disable-next-line no-underscore-dangle
-                    payload: dataJSON._embedded.episodes,
-                });
-            } catch (err) {
-                setLoading(false);
-                setError(true);
-                return false;
-            }
-        }
         if (loading === false && error === false && dispatch !== false) {
-            fetchEpisodes();
+            FetchEpisodes(
+                setLoading,
+                setError,
+                dispatch,
+                constants.URL_RICK_MORTY_EPISODES
+            );
         }
     }, [dispatch]);
     return [loading, error];
---
+++ src/services/fetchEpisodes.js
@@ -0,0 +1,18 @@
+const FetchEpisodes = async (setLoading, setError, dispatch, URL) => {
+    try {
+        const data = await fetch(URL);
+        const dataJSON = await data.json();
+        setLoading(true);
+        return dispatch({
+            type: 'FETCH_DATA',
+            // eslint-disable-next-line no-underscore-dangle
+            payload: dataJSON._embedded.episodes,
+        });
+    } catch (err) {
+        setLoading(false);
+        setError(true);
+        return false;
+    }
+};
+
+export default FetchEpisodes;
